b5a5299132878593f7834dc5260f319c46c82d11
Riocodex <onwukachibike@gmail.com>
2023-11-04 02:47:49 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("Rio")
   return (
     //beginnning of fragment
     <div className="App">
-      <h1>hello to fragments</h1>
+      <h1>{name}</h1>
     </div>//end of fragment
 
     //react fragments arebasically the group of code thats here..you cant have more than one fragments..you can also do <React.fragment/>but you cant use css withit..basically it 
+
+    //stateless vs stateful componets 
+    //a stateless componenet is a componnent with no state variables example(<div className="App">
+      //<h1>hello to fragments</h1>
+     // </div>)) whie stateful is one with state like the code above
+
   );
 }
 
